7aac124988da6d75f1bdef635340963fb94249b0
María Angeles Alonso <160015320+AngelesAlonso@users.noreply.github.com>
2026-01-12 09:36:28 +0100

    Update par_impar.py

--- par_impar.py
+++ par_impar.py
@@ -4,3 +4,10 @@ if numero % 2 == 0:
     print("El numeros que has indicado es par")
 else:
     print("El númeo indicado es impar")
+
+# versión 2 del enuncionado.
+# Mostrar por pantalla los números pares entre el 1 y el 20 (ambos incluidos)
+for n in range(1, 21):
+    es_par = n % 2 == 0
+    if es_par:
+        print(n)
