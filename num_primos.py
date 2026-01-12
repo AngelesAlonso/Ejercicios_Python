a665a7d907f7d34198e7303ab83bbc1fbfaed706
María Angeles Alonso <160015320+AngelesAlonso@users.noreply.github.com>
2026-01-12 13:01:11 +0100

    Update num_primos.py

--- num_primos.py
+++ num_primos.py
@@ -1,9 +1,23 @@
 numero = int(input("Escriba un número: "))
 
-for primo in range(2, numero + 1):
+for primo in range(1, numero + 1):
     if primo > 1:
         for i in range(2,primo):
             if primo % i == 0:
                 break
         else:
             print(primo)
+
+# Hacerlo con funcion
+'''
+numero = int(input("Escriba un número: "))
+
+def es_primo(n):
+    if n <= 1:       
+        return False
+    for i in range(2, n):   
+        if n % i == 0:        
+            return False
+    return Tre
+
+'''
