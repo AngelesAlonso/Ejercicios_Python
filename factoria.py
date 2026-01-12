597aff9bee528a3f4643dd1c5461051f4637cfb9
María Angeles Alonso <160015320+AngelesAlonso@users.noreply.github.com>
2026-01-12 10:32:34 +0100

    Update factoria.py

--- factoria.py
+++ factoria.py
@@ -2,7 +2,17 @@ numero = int(input("Introduzca un número: "))
 
 factorial = 1
 
-for i in range(1, numero +1):
+for i in range(1, numero +1):    # for i in range(2, numero +1):
     factorial *= i
     
 print(f"El factoria de {numero} es: {factorial}")
+
+#con función:
+
+def factorial (numero):
+    if numero == 0 or numero ==1:
+        return 1
+    else:
+        return factorial (numero-1) * numero
+
+print(factorial(int(input("Introduzca un número: "))))
